bd229626e20864ab705307b034e9f5fb367aacb8
ELIJAH Kinyamal <94557540+elijahkinyamal@users.noreply.github.com>
2023-11-14 23:02:55 +0300

    Update screenheader.style.js

--- components/common/header/screenheader.style.js
+++ components/common/header/screenheader.style.js
@@ -4,8 +4,8 @@ import { COLORS, SIZES } from "../../../constants";
 
 const styles = StyleSheet.create({
   btnContainer: {
-    width: 40,
-    height: 40,
+    width: 41,
+    height: 41,
     backgroundColor: COLORS.white,
     borderRadius: SIZES.small / 1.25,
     justifyContent: "center",
